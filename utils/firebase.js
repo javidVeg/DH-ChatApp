78ad8435ad1deb6a7d7d1df713e3c18507186f12
David_Martinez <javidx24@icloud.com>
2023-02-20 22:25:16 -0500

    removed data

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,11 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -18,16 +14,7 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDP1r0Fzgnui19_U4toIBiyiqS35gzI2lM",
-//   authDomain: "dh-chatapp.firebaseapp.com",
-//   projectId: "dh-chatapp",
-//   storageBucket: "dh-chatapp.appspot.com",
-//   messagingSenderId: "122607972450",
-//   appId: "1:122607972450:web:59ecd12904124fce8b613b",
-//   measurementId: "G-VWWPMCH8WN"
-// };
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 export const auth = getAuth();
